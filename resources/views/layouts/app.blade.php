ca0f16157ed1ed27fe8442b8d3eb80eb196a62f4
dotMavriQ <social@dotmavriq.life>
2026-01-25 11:33:16 +0000

    Fix page header bar to use theme background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-theme-bg-primary shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
