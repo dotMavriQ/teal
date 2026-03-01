f3933636200f5d2b80ac4ae058d4ae65dde27b0b
dotMavriQ <social@dotmavriq.life>
2026-03-01 14:24:40 +0000

    test: add missing test macros and fix postgres compatibility regressions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        \Illuminate\Testing\TestResponse::macro('assertSeeVolt', function ($component) {
+            return $this->assertSee($component);
+        });
+
+        \Illuminate\Testing\TestResponse::macro('assertSeeLivewire', function ($component) {
+            return $this->assertSee($component);
+        });
+    }
 }
